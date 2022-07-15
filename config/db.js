2e7e085e593ab030bcafbc0ab74e0ca4ba60b3a4
Alejandro Fernández Martín <ale.fernandez612@gmail.com>
2022-07-15 17:08:47 +0200

    Fin configuraciones globales

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: process.env.HOSTNAME,
+    user: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    port: process.env.PORT,
+    database: process.env.DATABASE,
+    ssl: {
+        ca: fs.readFileSync(path.join(__dirname, process.env.SSL)),
+        rejectUnauthorized: true,
+        require: true,
+    }
+});
+
+global.db = pool;
