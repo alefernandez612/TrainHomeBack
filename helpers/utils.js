3ed05dc88bfadaca277221f3036e45dd7e046e87
Alejandro Fernández Martín <ale.fernandez612@gmail.com>
2022-07-16 11:40:34 +0200

    Creado fichero utils para las consultas.

---
+++ helpers/utils.js
@@ -0,0 +1,23 @@
+function executeQuery(sql, values = []) {
+    return new Promise((resolve, reject) => {
+        db.query(sql, values, (err, result) => {
+            if (err) return reject(err);
+            resolve(result);
+        });
+    });
+}
+
+function executeQueryOne(sql, values = []) {
+    return new Promise((resolve, reject) => {
+        db.query(sql, values, (err, result) => {
+            if (err) return reject(err);
+            if (result.length === 0) return resolve(null);
+            resolve(result[0]);
+        });
+    });
+}
+
+module.exports = {
+    executeQuery,
+    executeQueryOne
+};
