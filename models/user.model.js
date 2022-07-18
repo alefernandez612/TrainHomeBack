7698c08db9d7f630fdee4b86100f8c92a97a6004
Alejandro Fernández Martín <ale.fernandez612@gmail.com>
2022-07-18 13:35:19 +0200

    Eliminadas funciones no usables en user.model y finalizado api/users.js

--- models/user.model.js
+++ models/user.model.js
@@ -20,41 +20,13 @@ const getById = (id) => {
 };
 
 /**
- * Devuelve el resultado de ejecutar la consulta "select * from user where email =?" con el parámetro
- * email.
- * @param email - la dirección de correo electrónico del usuario
- * @returns El resultado de la consulta.
- */
-const getByEmail = (email) => {
-    return executeQueryOne('select * from user where email = ?', [email]);
-};
-
-/**
- * Obtenga todos los usuarios que tengan al menos la edad especificada.
- * @param age - la edad del usuario
- * @returns El resultado de la consulta.
- */
-const getByAge = (age) => {
-    return executeQuery('select * from user where TIMESTAMPDIFF(YEAR, birthday, CURDATE()) >= ?)', [age]);
-};
-
-/**
- * Obtener todos los usuarios con un peso mayor o igual al peso dado.
- * @param weight - El peso del usuario
- * @returns Una promesa.
- */
-const getByWeight = (weight) => {
-    return executeQuery('select * from user where weight >= ?)', [weight]);
-};
-
-/**
  * Toma un objeto con las propiedades nombre de usuario, contraseña, correo electrónico, nombre,
  * apellido, cumpleaños, dirección, teléfono, sexo, altura, peso, avatar y los inserta en la base de
  * datos.
  * @returns El resultado de la consulta.
  */
-const create = ({ username, password, email, name, lastname, birthday, address, phone, gender, height, weight, avatar }) => {
-    return executeQuery('insert into user (username, password, email, name, lastname, birthday, address, phone, gender, height, weight, avatar) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [username, password, email, name, lastname, birthday, address, phone, gender, height, weight, avatar]);
+const create = ({ username, password, email, name, lastname, birthday, address, phone, gender, height, weight, avatar, objetive_id }) => {
+    return executeQuery('insert into user (username, password, email, name, lastname, birthday, address, phone, gender, height, weight, avatar, objetive_id) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [username, password, email, name, lastname, birthday, address, phone, gender, height, weight, avatar, objetive_id]);
 };
 
 /**
@@ -62,8 +34,8 @@ const create = ({ username, password, email, name, lastname, birthday, address, 
  * @param id - 1
  * @returns El resultado de la consulta.
  */
-const update = (id, { password, email, name, lastname, birthday, address, phone, gender, height, weight, avatar }) => {
-    return executeQuery('update user set password = ?, email = ?, name = ?, lastname = ?, birthday = ?, address = ?, phone = ?, gender = ?, height = ?, weight = ?, avatar = ? where id = ?', [password, email, name, lastname, birthday, address, phone, gender, height, weight, avatar, id]);
+const update = (id, { password, email, name, lastname, birthday, address, phone, gender, height, weight, avatar, objetive_id }) => {
+    return executeQuery('update user set password = ?, email = ?, name = ?, lastname = ?, birthday = ?, address = ?, phone = ?, gender = ?, height = ?, weight = ?, avatar = ?, objetive_id = ? where id = ?', [password, email, name, lastname, birthday, address, phone, gender, height, weight, avatar, objetive_id, id]);
 };
 
 /**
@@ -78,9 +50,6 @@ const deleteById = (id) => {
 module.exports = {
     getAll,
     getById,
-    getByAge,
-    getByEmail,
-    getByWeight,
     create,
     update,
     deleteById
--- routes/api/users.js
+++ routes/api/users.js
@@ -10,4 +10,41 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const user = await User.getById(id);
+        res.json(user);
+    } catch (err) {
+        res.json({ error: err.message });
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const { insertId } = await User.create(req.body);
+        const user = await User.getById(insertId);
+        res.json(user);
+    } catch (err) {
+        res.json({ err: err.message });
+    }
+});
+
+router.put('/:userId', (req, res) => {
+    const { userId } = req.params;
+    User.update(userId, req.body)
+        .then(() => {
+            res.json({ success: 'El usuario se ha actualizado correctamente.' });
+        }).catch((err) => {
+            res.json({ error: err.message });
+        });
+});
+
+router.delete('/:userId', (req, res) => {
+    const { userId } = req.params;
+    User.deleteById(userId)
+        .then(() => res.json({ success: 'El usuario se ha eliminado correctamente' }))
+        .catch((err) => res.json({ error: err.message }));
+});
+
 module.exports = router;
