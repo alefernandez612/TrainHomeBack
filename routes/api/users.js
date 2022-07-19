c0982ffbf33faa423ac9b5021992a5b6e4e3f320
Alejandro Fernández Martín <ale.fernandez612@gmail.com>
2022-07-19 18:06:29 +0200

    Estado actual: registro sin terminar, faltan middlewares.

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const { body, validationResult } = require('express-validator');
+const { createToken } = require('../../helpers/utils');
+const { checkToken } = require('../../helpers/middlewares');
 const User = require('../../models/user.model');
 
-router.get('/', async (req, res) => {
+router.get('/', checkToken, async (req, res) => {
     try {
         const users = await User.getAll();
         res.json(users);
@@ -20,18 +24,85 @@ router.get('/:id', async (req, res) => {
     }
 });
 
-router.post('/', async (req, res) => {
-    try {
-        const { insertId } = await User.create(req.body);
-        const user = await User.getById(insertId);
-        res.json(user);
-    } catch (err) {
-        res.json({ err: err.message });
+// { username, password, email, name, lastname, birthday, address, phone, gender, height, weight, avatar, objetive_id }
+
+router.post('/registro',
+
+    body('username')
+        .exists()
+        .withMessage("El campo username es requerido.")
+        .isLength({ min: 3, max: 15 })
+        .withMessage("El campo username debe tener mínimo 3 caracteres y máximo 15 caracteres.")
+        .custom(value => User.getByUsername(value).then(user => {
+            if (user) {
+                return Promise.reject('El nombre de usuario ya existe.');
+            }
+        })),
+
+    body('password')
+        .exists()
+        .withMessage("El campo password es requerido.")
+        .isLength({ min: 8, max: 20 })
+        .withMessage("El campo password debe tener mínimo 8 caracteres y máximo 20 caracteres."),
+
+    // body('username')
+    //     .exists()
+    //     .withMessage("El campo username es requerido.")
+    //     .isLength({ min: 3, max: 15 })
+    //     .withMessage("El campo username debe tener mínimo 3 caracteres y máximo 15 caracteres.")
+    //     .custom(value => User.getByUsername(value).then(user => {
+    //         if (user) {
+    //             return Promise.reject('El nombre de usuario ya existe.');
+    //         }
+    //     })),
+
+    // body('username')
+    //     .exists()
+    //     .withMessage("El campo username es requerido.")
+    //     .isLength({ min: 3, max: 15 })
+    //     .withMessage("El campo username debe tener mínimo 3 caracteres y máximo 15 caracteres.")
+    //     .custom(value => User.getByUsername(value).then(user => {
+    //         if (user) {
+    //             return Promise.reject('El nombre de usuario ya existe.');
+    //         }
+    //     })),
+
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.json(errors.array());
+        }
+        try {
+            req.body.password = bcrypt.hashSync(req.body.password, 10);
+            const { insertId } = await User.create(req.body);
+            const user = await User.getById(insertId);
+            res.json(user);
+        } catch (err) {
+            res.json({ error: err.message });
+        }
+    });
+
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    const user = await User.getByUsername(username);
+    if (!user) {
+        return res.json({ error: 'Usuario y/o contraseña incorrectos.' });
     }
+    const iguales = bcrypt.compareSync(password, user.password);
+    if (!iguales) {
+        return res.json({ error: 'La contraseña no es correcta' });
+    }
+
+    res.json({
+        success: 'Login correcto',
+        token: createToken(user)
+    });
 });
 
 router.put('/:userId', (req, res) => {
     const { userId } = req.params;
+    req.body.password = bcrypt.hashSync(req.body.password, 10);
     User.update(userId, req.body)
         .then(() => {
             res.json({ success: 'El usuario se ha actualizado correctamente.' });
