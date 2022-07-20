9e7570996210a9965147fb092382b461b7a5533f
Alejandro Fernández Martín <ale.fernandez612@gmail.com>
2022-07-20 20:28:13 +0200

    Pequeñas modificaciones en el código.

--- routes/api/routines.js
+++ routes/api/routines.js
@@ -2,80 +2,81 @@ const router = require('express').Router();
 const Routine = require('../../models/routine.model');
 const { checkAdmin } = require('../../helpers/middlewares');
 
-router.get('/', async (req, res) => {
-    try {
-        const routines = await Routine.getAll();
-        res.json(routines);
-    } catch (err) {
-        res.json({ error: err.message });
-    }
-});
-
-router.get('/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const routine = await Routine.getById(id);
-        res.json(routine);
-    } catch (err) {
-        res.json({ error: err.message });
-    }
-});
-
-router.get('/objetive/:objetive', async (req, res) => {
-    try {
-        const { objetive } = req.params;
-        const routine = await Routine.getByObjetive(objetive);
-        res.json(routine);
-    } catch (err) {
-        res.json({ error: err.message });
-    }
-});
-
-router.get('/muscle/:muscle', async (req, res) => {
-    try {
-        const { muscle } = req.params;
-        const routine = await Routine.getByMuscle(muscle);
-        res.json(routine);
-    } catch (err) {
-        res.json({ error: err.message });
-    }
-});
-
-router.get('/difficulty/:difficulty', async (req, res) => {
-    try {
-        const { difficulty } = req.params;
-        const routine = await Routine.getByDifficulty(difficulty);
-        res.json(routine);
-    } catch (err) {
-        res.json({ error: err.message });
-    }
-});
-
-router.post('/', async (req, res) => {
-    try {
-        const { insertId } = await Routine.create(req.body);
-        const routine = await Routine.getById(insertId);
-        res.json(routine);
-    } catch (err) {
-        res.json({ err: err.message });
-    }
-});
-
-router.put('/:routineId', checkAdmin, (req, res) => {
-    const { routineId } = req.params;
-    Routine.update(routineId, req.body)
-        .then(() => {
-            res.json({ success: 'La rutina se ha actualizado correctamente.' });
-        }).catch((err) => {
+router
+    .get('/', async (req, res) => {
+        try {
+            const routines = await Routine.getAll();
+            res.json(routines);
+        } catch (err) {
             res.json({ error: err.message });
-        });
-});
+        }
+    })
 
-router.delete('/:routineId', checkAdmin, (req, res) => {
-    const { routineId } = req.params;
-    Routine.deleteById(routineId)
-        .then(() => res.json({ success: 'La rutina se ha eliminado correctamente' }))
-        .catch((err) => res.json({ error: err.message }));
-});
+    .get('/:id', async (req, res) => {
+        try {
+            const { id } = req.params;
+            const routine = await Routine.getById(id);
+            res.json(routine);
+        } catch (err) {
+            res.json({ error: err.message });
+        }
+    })
+
+    .get('/objetive/:objetive', async (req, res) => {
+        try {
+            const { objetive } = req.params;
+            const routine = await Routine.getByObjetive(objetive);
+            res.json(routine);
+        } catch (err) {
+            res.json({ error: err.message });
+        }
+    })
+
+    .get('/muscle/:muscle', async (req, res) => {
+        try {
+            const { muscle } = req.params;
+            const routine = await Routine.getByMuscle(muscle);
+            res.json(routine);
+        } catch (err) {
+            res.json({ error: err.message });
+        }
+    })
+
+    .get('/difficulty/:difficulty', async (req, res) => {
+        try {
+            const { difficulty } = req.params;
+            const routine = await Routine.getByDifficulty(difficulty);
+            res.json(routine);
+        } catch (err) {
+            res.json({ error: err.message });
+        }
+    })
+
+    .post('/', async (req, res) => {
+        try {
+            const { insertId } = await Routine.create(req.body);
+            const routine = await Routine.getById(insertId);
+            res.json(routine);
+        } catch (err) {
+            res.json({ err: err.message });
+        }
+    })
+
+    .put('/:routineId', checkAdmin, (req, res) => {
+        const { routineId } = req.params;
+        Routine.update(routineId, req.body)
+            .then(() => {
+                res.json({ success: 'La rutina se ha actualizado correctamente.' });
+            }).catch((err) => {
+                res.json({ error: err.message });
+            });
+    })
+
+    .delete('/:routineId', checkAdmin, (req, res) => {
+        const { routineId } = req.params;
+        Routine.deleteById(routineId)
+            .then(() => res.json({ success: 'La rutina se ha eliminado correctamente' }))
+            .catch((err) => res.json({ error: err.message }));
+    });
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -5,89 +5,90 @@ const { createToken } = require('../../helpers/utils');
 const { checkToken, checkAdmin } = require('../../helpers/middlewares');
 const User = require('../../models/user.model');
 
-router.get('/', checkToken, checkAdmin, async (req, res) => {
-    try {
-        const users = await User.getAll();
-        res.json(users);
-    } catch (err) {
-        res.json({ error: err.message });
-    }
-});
-
-router.get('/:id', checkToken, async (req, res) => {
-    try {
-        const { id } = req.params;
-        const user = await User.getById(id);
-        res.json(user);
-    } catch (err) {
-        res.json({ error: err.message });
-    }
-});
-
-router.post('/registro',
-
-    body('username')
-        .custom(value => User.getByUsername(value).then(user => {
-            if (user) {
-                return Promise.reject('El nombre de usuario ya existe.');
-            }
-        })),
-    body('email')
-        .custom(value => User.getByEmail(value).then(email => {
-            if (email) {
-                return Promise.reject('El email ya existe.');
-            }
-        })),
-
-    async (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.json(errors.array());
-        }
+router
+    .get('/', checkToken, checkAdmin, async (req, res) => {
         try {
-            req.body.password = bcrypt.hashSync(req.body.password, 10);
-            const { insertId } = await User.create(req.body);
-            const user = await User.getById(insertId);
+            const users = await User.getAll();
+            res.json(users);
+        } catch (err) {
+            res.json({ error: err.message });
+        }
+    })
+
+    .get('/:id', checkToken, async (req, res) => {
+        try {
+            const { id } = req.params;
+            const user = await User.getById(id);
             res.json(user);
         } catch (err) {
             res.json({ error: err.message });
         }
-    });
+    })
 
-router.post('/login', async (req, res) => {
-    const { username, password } = req.body;
+    .post('/registro',
 
-    const user = await User.getByUsername(username);
-    if (!user) {
-        return res.json({ error: 'Usuario y/o contraseña incorrectos.' });
-    }
-    const iguales = bcrypt.compareSync(password, user.password);
-    if (!iguales) {
-        return res.json({ error: 'La contraseña no es correcta' });
-    }
+        body('username')
+            .custom(value => User.getByUsername(value).then(user => {
+                if (user) {
+                    return Promise.reject('El nombre de usuario ya existe.');
+                }
+            })),
+        body('email')
+            .custom(value => User.getByEmail(value).then(email => {
+                if (email) {
+                    return Promise.reject('El email ya existe.');
+                }
+            })),
 
-    res.json({
-        success: 'Login correcto',
-        token: createToken(user)
-    });
-});
+        async (req, res) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.json(errors.array());
+            }
+            try {
+                req.body.password = bcrypt.hashSync(req.body.password, 10);
+                const { insertId } = await User.create(req.body);
+                const user = await User.getById(insertId);
+                res.json(user);
+            } catch (err) {
+                res.json({ error: err.message });
+            }
+        })
 
-router.put('/:userId', checkToken, (req, res) => {
-    const { userId } = req.params;
-    req.body.password = bcrypt.hashSync(req.body.password, 10);
-    User.update(userId, req.body)
-        .then(() => {
-            res.json({ success: 'El usuario se ha actualizado correctamente.' });
-        }).catch((err) => {
-            res.json({ error: err.message });
+    .post('/login', async (req, res) => {
+        const { username, password } = req.body;
+
+        const user = await User.getByUsername(username);
+        if (!user) {
+            return res.json({ error: 'Usuario y/o contraseña incorrectos.' });
+        }
+        const iguales = bcrypt.compareSync(password, user.password);
+        if (!iguales) {
+            return res.json({ error: 'La contraseña no es correcta' });
+        }
+
+        res.json({
+            success: 'Login correcto',
+            token: createToken(user)
         });
-});
+    })
 
-router.delete('/:userId', checkToken, (req, res) => {
-    const { userId } = req.params;
-    User.deleteById(userId)
-        .then(() => res.json({ success: 'El usuario se ha eliminado correctamente' }))
-        .catch((err) => res.json({ error: err.message }));
-});
+    .put('/:userId', checkToken, (req, res) => {
+        const { userId } = req.params;
+        req.body.password = bcrypt.hashSync(req.body.password, 10);
+        User.update(userId, req.body)
+            .then(() => {
+                res.json({ success: 'El usuario se ha actualizado correctamente.' });
+            }).catch((err) => {
+                res.json({ error: err.message });
+            });
+    })
+
+    .delete('/:userId', checkToken, (req, res) => {
+        const { userId } = req.params;
+        User.deleteById(userId)
+            .then(() => res.json({ success: 'El usuario se ha eliminado correctamente' }))
+            .catch((err) => res.json({ error: err.message }));
+    });
 
 module.exports = router;
