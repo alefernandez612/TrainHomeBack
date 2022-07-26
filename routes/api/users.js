7e76a21d9ce7722e9266a415c699d8187726da71
Alejandro Fernández Martín <ale.fernandez612@gmail.com>
2022-07-26 13:22:27 +0200

    Upload creado.

--- routes/api/users.js
+++ routes/api/users.js
@@ -37,7 +37,7 @@ router
         }
     })
 
-    .post('/register', upload.single('imagen'),
+    .post('/register', upload.single('avatar'),
 
         body('username')
             .custom(value => User.getByUsername(value).then(user => {
@@ -57,10 +57,11 @@ router
             if (!errors.isEmpty()) {
                 return res.json(errors.array());
             }
-
+            console.log(req.file);
             const extension = '.' + req.file.mimetype.split('/')[1];
             const newAvatar = req.file.filename + extension;
             const newPath = req.file.path + extension;
+            console.log(newPath);
 
             fs.renameSync(req.file.path, newPath);
             req.body.avatar = newAvatar;
